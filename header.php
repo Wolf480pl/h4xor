064494eedcfdf12bca29917c0579449eedfd7f0f
Wolf480pl <wolf480@interia.pl>
2014-05-09 21:54:44 +0200

    Let's make it HTML 5. * moved the XFN profile from <head> to <link> * the validator will still complain about the archives... they may be included in HTML5, so let's leave them for now.

--- header.php
+++ header.php
@@ -1,10 +1,11 @@
 <?php
 #
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
 
-<head profile="http://gmpg.org/xfn/11">
+<head>
+    <link rel="profile" href="http://gmpg.org/xfn/11"/>
 	<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
 
 	<title><?php bloginfo('name'); ?> <?php if ( is_single() ) { ?> &raquo; Blog Archive <?php } ?> <?php wp_title(); ?></title>
